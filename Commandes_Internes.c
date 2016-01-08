b43e6bef47e530aac9373e0df39fec3a8f532ce7
Clement Nerestan <clenerestan@emi.u-bordeaux1.fr>
2016-01-08 17:52:38 +0100

    Avancé sur les commandes internes

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -1,6 +1,27 @@
+#define _GNU_SOURCE
 #include "Commandes_Internes.h"
 #include "unistd.h"
 #include "signal.h"
+#include "stdio.h"
+
+/*
+Fonction utilitaire 
+*/
+
+char* getParam(char* c){
+  char* param;
+  int i = 1;
+  while(c[i] != '\0'){
+    param[i-1] = c[i];
+    i++;
+  }
+  return param;
+}
+
+
+/*
+Fonction Interne
+*/
 
 void cmdExit(char** c){ //TODO notice me sempai Pellegrini
   int n = 0;
@@ -11,14 +32,43 @@ void cmdExit(char** c){ //TODO notice me sempai Pellegrini
 
 
 
-void cmdKill(char** c){
+/*void cmdKill(char** c){
   int n = 0;
   if(c[0] != 0){
     n = atoi(c[0]);
     kill(n,SIGKILL);
   }
+  }*/
+
+void cmdKill(char** c){
+  int n;
+  int opt;
+  char* param;
+  if(c[0] == 0)
+    return ;
+  int i =0;
+  while (c[i][0] == '-'){
+    param = getParam(c[i]);
+  }
+
 }
 
 void cmdEcho(char** c){
-  write(STDOUT_FILENO,c,sizeof(c));
+  printf("%s",c);
+}
+
+void cmdCd(char** c){
+  char* dir;
+  char* tmp = get_current_dir_name();
+  strcat(dir,tmp);
+  free(tmp);
+  strcat(dir,c[0]);
+  chdir(dir);
+  perror("cd");
+}
+
+void cmdPwd(char** c){
+  char* tmp = get_current_dir_name();
+  printf("%s",tmp);
+  free(tmp);
 }
