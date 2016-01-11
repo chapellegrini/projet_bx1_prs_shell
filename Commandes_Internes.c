75f478cfc17ec9507447796f992890015cee8071
Clement Nerestan <clenerestan@emi.u-bordeaux1.fr>
2016-01-11 21:50:40 +0100

    Ajout de commentaire

--- Commandes_Internes.c
+++ Commandes_Internes.c
@@ -10,9 +10,8 @@
 
 
 /*
-Fonction Interne
+Effectue un appel system exit avec l'argument donné en paramètre lors de l'appel de la commande
 */
-
 int cmdExit(char** c){ //TODO notice me sempai Pellegrini
   int n = 0;
   if(c[0] != 0)
@@ -22,7 +21,7 @@ int cmdExit(char** c){ //TODO notice me sempai Pellegrini
 
 
 
-
+/*Envoie le signal donné en paramètre au PID souhaité*/
 int cmdKill(char** c){
   int n;
   int opt;
@@ -35,7 +34,7 @@ int cmdKill(char** c){
     printf("%s \n",param);
     i++;
   }
-  if (strcmp(param,"l")==0){
+  if (strcmp(param,"l")==0){ /*gestion de l'option "-l" */
     printf( "1 HUP \n2 INT \n3 QUIT \n4 ILL \n5 TRAP \n6 ABRT \n7 BUS \n8 FPE \n9 KILL \n10 USR1 \n11 SEGV \n12 USR2 \n13 PIPE \n14 ALRM \n15 TERM \n16 STKFLT \n17 CHLD \n18 CONT \n19 STOP \n20 TSTP \n21 TTIN \n22 TTOU \n23 URG \n24 XCPU \n25 XFSZ \n26 VTALRM \n27 PROF \n28 WINCH \n29 POLL \n30 PWR \n31 SYS \n");
     }
   while(c[i] != 0){
@@ -46,7 +45,7 @@ int cmdKill(char** c){
   }
   return EXIT_SUCCESS;
 }
-
+/* Ecrit sur la sortie standard les arguments*/
 int cmdEcho(char** c){
   int i = 0;
   while(c[i] != 0)
@@ -54,30 +53,38 @@ int cmdEcho(char** c){
   return EXIT_SUCCESS;
 }
 
+/*appel de chdir avec l'argument de la commande*/
 int cmdCd(char** c){
   chdir(c[0]);
   perror("cd");
   return EXIT_SUCCESS;
 }
 
+/* Affiche le répertoire courant sur la sortie standard */
 int cmdPwd(char** c){
   char* t = get_current_dir_name();
   printf("%s",t);
   free(t);
   return EXIT_SUCCESS;
 }
-
+/*Affiche sur la sortie standard le hostname du systeme */
 int cmdHostname(char** c){
-  char name[256];
-  int i = gethostname(name, sizeof(name));
-  if(i!=0)
-    perror("errno");
+  if(c[0]==0){
+    char name[256];
+    int i = gethostname(name, sizeof(name));
+    if(i!=0)
+      perror("gethostname");
+    else{
+      printf("%s",name);
+    } 
+  }
   else{
-    printf("%s",name);
+    sethostname(c[0],sizeof(c[0]));
+    perror("sethostname");
   }
   return EXIT_SUCCESS;
 }
-
+/* Affiche sur la sortie standard la date au format courant en France */
 int cmdDate(char** c){
   time_t t = time(NULL);
   struct tm *date = localtime(&t);
@@ -90,6 +97,8 @@ int cmdDate(char** c){
   return EXIT_SUCCESS;
 }
 
+
+/* Affiche l'historique des commande entrées dans le Shell lors de la session*/
 int cmdHistory(char** c){
   HIST_ENTRY **history = history_list();
   for(int i = 0;history[i] != NULL; i++){
